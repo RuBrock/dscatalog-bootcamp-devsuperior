35ac69178ccda8168d6d1612cd07e2fb5b1721ea
Rubens Brock <r.brocksilva@yahoo.com.br>
2020-11-18 22:33:57 -0300

    Add allowed roles on private router

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
+import PrivateRoute from 'core/components/Routes/PrivateRoute';
 import './styles.scss';
 
 const Admin = () => (
@@ -11,17 +12,17 @@ const Admin = () => (
         <div className="admin-content">
             <Switch>
 
-                <Route path="/admin/products">
+                <PrivateRoute path="/admin/products">
                     <Products />
-                </Route>
+                </PrivateRoute>
 
-                <Route path="/admin/categories">
+                <PrivateRoute path="/admin/categories">
                     <h1>Categories</h1>
-                </Route>
+                </PrivateRoute>
 
-                <Route path="/admin/users">
+                <PrivateRoute path="/admin/users" allowedRoutes={['ROLE_ADMIN']}>
                     <h1>Users</h1>
-                </Route>
+                </PrivateRoute>
 
             </Switch>
         </div>
