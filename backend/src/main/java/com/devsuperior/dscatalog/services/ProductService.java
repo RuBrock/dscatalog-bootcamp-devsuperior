127a9992b055746cf5a31de0802703155976099a
Rubens Brock <r.brocksilva@yahoo.com.br>
2021-02-08 20:45:59 -0300

    Ajuste provisorio de inserção de produtos

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -47,6 +47,11 @@ public class ProductService {
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
 		copyDtoToEntity(dto, entity);
+		if (entity.getCategories().size() == 0) {
+			Category cat = categoryRepository.getOne(1L);
+			entity.getCategories().add(cat);
+		}
+		
 		entity = repository.save(entity);
 		return new ProductDTO(entity);		
 	}
