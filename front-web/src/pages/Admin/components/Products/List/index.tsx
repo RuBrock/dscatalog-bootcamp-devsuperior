1bc975e22b52c2d77f850fdcf024b77d5573cae6
Rubens Brock <r.brocksilva@yahoo.com.br>
2021-02-21 14:08:47 -0300

    Fix product filters

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { ProductsResponse } from 'core/types/Product';
+import { Category, ProductsResponse } from 'core/types/Product';
 import { makePrivateRequest, makeRequest } from 'core/utils/request';
 import Pagination from 'core/components/Pagination';
 import Card from '../Card';
 import CardLoader from '../Loaders/ProductCardLoader';
+import ProductFilters from 'core/components/ProductFilters';
 import './styles.scss';
 
 const List = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
     const [activePage, setActivePage] = useState(0);
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState<Category>();
     const history = useHistory();
 
     const getProducts = useCallback(() => {
@@ -19,7 +22,9 @@ const List = () => {
             page: activePage,
             linesPerPage: 4,
             direction: 'DESC',
-            orderBy: 'id'
+            orderBy: 'id',
+            name,
+            categoryId: category?.id
         }
 
         setIsLoading(true);
@@ -28,7 +33,7 @@ const List = () => {
             .finally(() => {
                 setIsLoading(false);
             })
-    }, [activePage])
+    }, [activePage, name, category])
 
     useEffect(() => {
         getProducts();
@@ -53,11 +58,38 @@ const List = () => {
         }
     }
 
+    const handleChangeName = (name: string) => {
+        setActivePage(0);
+        setName(name);
+    }
+
+    const handleChangeCategory = (category: Category) => {
+        setActivePage(0);
+        setCategory(category);
+    }
+
+    const clearFilters = () => {
+        setActivePage(0);
+        setCategory(undefined);
+        setName('');
+    }
+
     return (
         <div className="admin-products-list">
-            <button className="btn btn-primary btn-lg" onClick={handleCreate}>
-                ADICIONAR
-            </button>
+
+            <div className="d-flex justify-content-between">
+                <button className="btn btn-primary btn-lg" onClick={handleCreate}>
+                    ADICIONAR
+                </button>
+
+                <ProductFilters  
+                    name={name}
+                    category={category}
+                    handleChangeName={handleChangeName}
+                    handleChangeCategory={handleChangeCategory}
+                    clearFilters={clearFilters}
+                />
+            </div>
 
             <div className="admin-list-container">
                 { 
